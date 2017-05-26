2df43f9d49df13aa418d2c4a29c48b706164767b
Aaron <aaronmeade9292@gmail.com>
2017-05-25 19:27:47 -0700

    So far so good

--- app.js
+++ app.js
@@ -8,4 +8,20 @@ if (confirmed === true) {
 else {
   console.log('The user said they are not ready... but we are going to start anyways');
 }
-alert('This is a test!')
+var answer1 = prompt('Do you enjoy beer? \(This is a yes or no question, so please answer with a Y or N.\)');
+console.log("Of course you do! Who doesn\'t");
+
+if (answer1.toUpperCase() === 'Y') {
+  alert('Of course you do! Everyone does!')
+}
+else {
+  alert('Alright I guess that\'s cool')
+}
+
+var answer1 = prompt(user + ' this is a yes or no question, so please answer with a Y or N. Does Adam have a dog?');
+
+if (answer1.toUpperCase() === 'Y') {
+  alert('Yes! You are correct! Adam does have a dog!');
+} else {
+  alert('Nope... too bad.');
+}
