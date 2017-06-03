d2dd57aefccf943d95ed9d6aade1738924d92dda
Aaron <aaronmeade9292@gmail.com>
2017-06-03 11:02:42 -0700

    acp

--- app.js
+++ app.js
@@ -13,46 +13,61 @@ var answer1 = prompt('\(Some of these questions will be yes or no questions, so 
 if (answer1.toUpperCase() === 'Y') {
   alert('Of course you do! Everyone does!');
 }
-else {
+
+  else {
   alert('Alright I guess that\'s cool');
+  console.log('Keep trying!');
 }
 
 var answer2 = prompt('Do you like Rick and Morty?');
 
 if (answer2.toUpperCase() === 'Y') {
   alert('And that\'s the wayyyyyy the news goes');
+  console.log('2/5 correct!');
 }
 else {
   alert('Graaassssss taste bad');
+  console.log('Keep trying!');
 }
 
 var answer1 = prompt('Are Black Bears the best bears?');
 
 if (answer1.toUpperCase() === 'Y') {
   alert('FACT: Bears eat beets');
+  console.log('3/5 correct!');
 }
 else {
   alert('That\'s debatable');
+  console.log('Keep trying!');
 }
 
 var answer1 = prompt('Are those mountains on a cob?');
 
 if (answer1.toUpperCase() === 'Y') {
     alert('Get in the ship sweetie!');
+    console.log('4/5 correct')
 }
 
 else {
   alert('Ok we can stay here');
+  console.log('Keep trying!');
 }
 
 var answer1 = prompt('Do you like RuPaul?');
 
 if (answer1.toUpperCase() === 'Y') {
   alert('Shantay you stay');
+  correct++;
 }
 
 else {
   alert('Sashay away');
+}
+
+var answer1 = prompt('How many different color Lantern Corps. are there?')
+
+if (answer1 === 8) {
+  alert()
 }
 
 var answer1 = prompt('Can you guess another state that I have lived in besides Washington?');
